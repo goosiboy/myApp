d14f069eeb0f2f36b470b39583aec2d43e10705c
Jony Oinas <jonyoinas@gmail.com>
2018-03-08 12:39:32 +0200

    MEAN - frontend building. Form validation
    Angular-CLI broken

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  // We need to bring services into the component by initializing them in a constructor
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService
+  ) {}
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessage.show('Please fill in all fields', {
+        cssClass: 'alert-danger', timeout: 3000
+      });
+      return false;
+    }
+
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessage.show('Please use a valid email', {
+        cssClass: 'alert-danger', timeout: 3000
+      });
+      return false;
+    }
+
   }
 
 }
